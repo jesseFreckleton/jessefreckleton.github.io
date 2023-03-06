f44132186694a4548916b42fc7ff1113b808a355
jesseFreckleton <117680436+jesseFreckleton@users.noreply.github.com>
2023-03-06 00:57:16 -0700

    Add files via upload

---
+++ java_script/about.js
@@ -0,0 +1,11 @@
+"use strict";
+
+$("document").ready(() => {
+  
+  $("#accordion").accordion({
+    active: false,
+    event: "click",
+    heightStyle: "content",
+    collapsible: true
+  });
+});
