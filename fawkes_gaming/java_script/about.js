096aa871d9a8a56d3c2ed9d06728dabdc0752d5b
jesseFreckleton <117680436+jesseFreckleton@users.noreply.github.com>
2023-03-06 00:59:25 -0700

    Delete fawkes_gaming directory

--- fawkes_gaming/java_script/about.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-$("document").ready(() => {
-  
-  $("#faqs h2").click(evt => {
-    $(evt.currentTarget).toggleClass("minus");
-    $(evt.currentTarget).next().slideToggle();
-  });
-});
